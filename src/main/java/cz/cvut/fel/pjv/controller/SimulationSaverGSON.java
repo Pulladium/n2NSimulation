0a10f3541067449631179822f28d7b8cc44e855a
Artem Vozhov <vozhoart@fit.cvut.cz>
2024-05-18 15:51:47 +0200

    load

--- src/main/java/cz/cvut/fel/pjv/controller/SimulationSaverGSON.java
+++ src/main/java/cz/cvut/fel/pjv/controller/SimulationSaverGSON.java
@@ -2,6 +2,7 @@ package cz.cvut.fel.pjv.controller;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import cz.cvut.fel.pjv.jsPORT.Mover;
 import cz.cvut.fel.pjv.model.JsonAdapters.*;
 import cz.cvut.fel.pjv.model.SimulationState;
 import cz.cvut.fel.pjv.model.ecsComponents.CompColor;
@@ -38,12 +39,24 @@ public class SimulationSaverGSON {
     }
 
     public SimulationState loadSimStateGSON(String path2load) {
-        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation()
+                .registerTypeAdapter(CompColor.class, new ColorCompAdapter())
+                .registerTypeAdapter(MassComponent.class, new MassCompAdapter())
+                .registerTypeAdapter(Point2DExt.class , new Point2DExtAdapter()).
+        create();
 
         SimulationState state = null;
         try (FileReader reader = new FileReader(path2load)) {
             state = gson.fromJson(reader, SimulationState.class);
 
+
+            // Создание новых Entity для каждого Mover
+            for (Mover mover : state.getMovers()) {
+                mover.currentEntity = mover.newMover();
+            }
+            if (state.getSun() != null) {
+                state.getSun().currentEntity = state.getSun().newMover();
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
