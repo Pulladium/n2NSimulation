dfca4c243f1be5c2b4e4685927b759b6a464a5b4
Artem Vozhov <vozhoart@fit.cvut.cz>
2024-05-18 13:09:22 +0200

    porting to enging Atractor comp

--- src/main/java/cz/cvut/fel/pjv/model/ecsSystems/N2mAtraction.java
+++ src/main/java/cz/cvut/fel/pjv/model/ecsSystems/N2mAtraction.java
@@ -16,47 +16,62 @@ public class N2mAtraction extends EngineSystem {
     public N2mAtraction(ArrayList<Mover> movers) {
         this.movers = movers;
     }
-    public void draw(GraphicsContext gc) {
-        double WIDTH = gc.getCanvas().getWidth();
-        double HEIGHT = gc.getCanvas().getHeight();
 
 
-        gc.clearRect(0, 0, WIDTH, HEIGHT);
-        gc.setFill(Color.BLACK);
-        gc.fillRect(0, 0, WIDTH, HEIGHT);
+    @Override
+    public void update(double dT) {
 
-//TODO To engine UPS
-        for (Mover mover : movers) {
-            MoveableHandl moveableHandl = mover.currentEntity.getComponent(MoveableHandl.class);
-//            MoveableHandl sunMoveableHandl = sun.currentEntity.getComponent(MoveableHandl.class);
-
-
-//            sunMoveableHandl.attract(moveableHandl.getEntity());
-            for (Mover other : movers) {
-                if (mover != other) {
-                    MoveableHandl otherMoveableHandl = other.currentEntity.getComponent(MoveableHandl.class);
-                    moveableHandl.attract(otherMoveableHandl.getEntity());
-                }
-            }
-
-        }
-        //TODO To engine finish
-
-        //TODO ToCanvasRenderer FPS
-        gc.save();
-        gc.translate(WIDTH / 2, HEIGHT / 2);
-
-        for (Mover mover : movers) {
-            //changePos
-            MoveableHandl moveableHandl = mover.currentEntity.getComponent(MoveableHandl.class);
-            moveableHandl.update();
-            moveableHandl.show(gc);
-        }
-
-//        sun.currentEntity.getComponent(MoveableHandl.class).show(gc);
-
-        gc.restore();
-//        sun.show(gc, WIDTH / 2, HEIGHT / 2);
+        ////            sunMoveableHandl.attract(moveableHandl.getEntity());
+//            for (Mover other : movers) {
+//                if (mover != other) {
+//                    MoveableHandl otherMoveableHandl = other.currentEntity.getComponent(MoveableHandl.class);
+//                    moveableHandl.attract(otherMoveableHandl.getEntity());
+//                }
+//            }
+//
     }
+
+//    public void draw(GraphicsContext gc) {
+//        double WIDTH = gc.getCanvas().getWidth();
+//        double HEIGHT = gc.getCanvas().getHeight();
+//
+//
+//        gc.clearRect(0, 0, WIDTH, HEIGHT);
+//        gc.setFill(Color.BLACK);
+//        gc.fillRect(0, 0, WIDTH, HEIGHT);
+//
+////TODO To engine UPS
+//        for (Mover mover : movers) {
+//            MoveableHandl moveableHandl = mover.currentEntity.getComponent(MoveableHandl.class);
+//            MoveableHandl sunMoveableHandl = sun.currentEntity.getComponent(MoveableHandl.class);
+//
+//
+////            sunMoveableHandl.attract(moveableHandl.getEntity());
+//            for (Mover other : movers) {
+//                if (mover != other) {
+//                    MoveableHandl otherMoveableHandl = other.currentEntity.getComponent(MoveableHandl.class);
+//                    moveableHandl.attract(otherMoveableHandl.getEntity());
+//                }
+//            }
+//
+//        }
+//        //TODO To engine finish
+//
+//        //TODO ToCanvasRenderer FPS
+//        gc.save();
+//        gc.translate(WIDTH / 2, HEIGHT / 2);
+//
+//        for (Mover mover : movers) {
+//            //changePos
+//            MoveableHandl moveableHandl = mover.currentEntity.getComponent(MoveableHandl.class);
+//            moveableHandl.update();
+//            moveableHandl.show(gc);
+//        }
+//
+////        sun.currentEntity.getComponent(MoveableHandl.class).show(gc);
+//
+//        gc.restore();
+////        sun.show(gc, WIDTH / 2, HEIGHT / 2);
+//    }
 
 }
